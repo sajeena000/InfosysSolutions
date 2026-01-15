2fde2968621a0d22d0fcf417018b08c45b69583b
sajeena000 <sajeenamalla000@gmail.com>
2026-01-15 16:19:32 +0000

    feat: configure nuxt, tailwind and google fonts

---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from 'tailwindcss'
+
+export default <Config>{
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        display: ['Space Grotesk', 'sans-serif'],
+      },
+      colors: {
+        accent: {
+          500: '#6366f1',
+          600: '#4f46e5',
+        }
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      }
+    },
+  },
+  plugins: [],
+}
