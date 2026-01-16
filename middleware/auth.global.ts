69b09f52811a25ddf284340822596f46b37ad8de
sajeena000 <sajeenamalla000@gmail.com>
2026-01-16 15:07:07 +0000

    updated middleware/auth.global.ts

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -3,14 +3,11 @@ import { useAppStore } from '~/stores/appStore'
 export default defineNuxtRouteMiddleware((to, from) => {
   const store = useAppStore()
 
-  if (to.path === '/login') {
-    if (store.isAuthenticated) {
-      return navigateTo('/')
-    }
-    return
+  if (to.path === '/login' && store.isAuthenticated) {
+    return navigateTo('/')
   }
 
-  if (!store.isAuthenticated) {
+  if (to.path !== '/login' && !store.isAuthenticated) {
     return navigateTo('/login')
   }
 })
