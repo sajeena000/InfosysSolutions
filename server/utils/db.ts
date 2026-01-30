7189924b5b9d13f8e55f416c4e3e124399509c8f
sajeena000 <sajeenamalla000@gmail.com>
2026-01-30 04:26:16 +0000

    Updated db.ts

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,6 +1,6 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
-import * as schema from '../database/schema'; // Import your schema
+import * as schema from '../database/schema'; 
 
 const connectionString = process.env.DATABASE_URL!;
 
