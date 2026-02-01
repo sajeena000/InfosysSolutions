506e0f3fd85a28c94fd830c355136351a2f43cf8
sajeena000 <159105119+sajeena000@users.noreply.github.com>
2026-02-01 17:22:46 +0000

    Deploying to gh-pages from @ sajeena000/DashboardUI@07857485b40cb96ec0f1a93d0b5c751e85388157 🚀

--- _nuxt/CqshNR8s.js
+++
@@ -1,1 +0,0 @@
-import{c as e}from"./ME1vAZHe.js";const h=e("chevron-left",[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]]);const o=e("chevron-right",[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]]);export{h as C,o as a};
